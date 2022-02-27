2b1b88e9233f64f50ddcf26589ad2db9775dc510
hahnlee <hanlee.dev@gmail.com>
2022-02-27 13:40:54 +0900

    feat: add vimrc

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,6 +1,8 @@
 # Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install wget
+
 # ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -24,3 +26,6 @@ brew install vim
 
 ## Vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+## Update vimrc
+wget https://raw.githubusercontent.com/hahnlee/dotfiles/main/.vimrc -O ~/.vimrc
